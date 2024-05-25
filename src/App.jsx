efe5013db671995160006d22d638bb89246a8ec4
Kalsar <alfazkhan560@gmail.com>
2024-05-25 12:35:20 +0530

    start yt clone

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './components/navbar/Navbar'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+      
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className=''>
+
+    </nav>
+  )
+}
+
+export default Navbar
